b60d0a980e09035769b145cc14593ea99414550a
PGGopika <pggopika369@gmail.com>
2025-05-24 19:51:48 +0530

    Fix paths for Vercel deployment

--- server/ml/classify_gru.py
+++ server/ml/classify_gru.py
@@ -1,42 +1,5 @@
-# train_gru_model.py
-import numpy as np
-import tensorflow as tf
-from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Input, GRU, Dense
-
-# Simulated dataset (replace with your actual labeled data)
-# X.shape = (samples, time_steps, features)
-X = np.random.rand(500, 20, 10)  # 500 samples, 20 time steps, 10 sensor features
-y_activity = tf.keras.utils.to_categorical(np.random.randint(0, 4, 500), num_classes=4)
-y_emotion = tf.keras.utils.to_categorical(np.random.randint(0, 4, 500), num_classes=4)
-y_sleep = tf.keras.utils.to_categorical(np.random.randint(0, 3, 500), num_classes=3)
-
-# Model Architecture
-input_layer = Input(shape=(20, 10))
-x = GRU(64, return_sequences=False)(input_layer)
-x = Dense(64, activation='relu')(x)
-
-# Three output heads
-activity_out = Dense(4, activation='softmax', name='activity')(x)
-emotion_out = Dense(4, activation='softmax', name='emotion')(x)
-sleep_out = Dense(3, activation='softmax', name='sleep')(x)
-
-model = Model(inputs=input_layer, outputs=[activity_out, emotion_out, sleep_out])
-
-model.compile(optimizer='adam',
-              loss={'activity': 'categorical_crossentropy',
-                    'emotion': 'categorical_crossentropy',
-                    'sleep': 'categorical_crossentropy'},
-              metrics=['accuracy'])
-
-model.fit(X, [y_activity, y_emotion, y_sleep], epochs=20, batch_size=32)
-
-# Save model
-model.save("gru_model.h5")
-print("Model trained and saved.")
-
-
 # classify_gru.py
+
 import sys
 import json
 import numpy as np
@@ -45,11 +8,11 @@ import tensorflow as tf
 # Load the saved GRU model
 model = tf.keras.models.load_model("gru_model.h5")
 
-# Read JSON input from stdin
+# Read JSON input from stdin (sent from your Node.js server)
 raw_input = sys.stdin.read()
 sensor_data = json.loads(raw_input)
 
-# Convert single sensor data to a simulated time sequence
+# Expected input keys: accX, accY, accZ, gyroX, gyroY, gyroZ, heartRate, spo2, bodyTemp, gsr
 input_vector = np.array([
     sensor_data['accX'], sensor_data['accY'], sensor_data['accZ'],
     sensor_data['gyroX'], sensor_data['gyroY'], sensor_data['gyroZ'],
@@ -57,13 +20,14 @@ input_vector = np.array([
     sensor_data['bodyTemp'], sensor_data['gsr']
 ])
 
+# Repeat the vector to simulate a sequence of 20 time steps
 sequence = np.tile(input_vector, (20, 1))  # shape: (20, 10)
 sequence = sequence.reshape(1, 20, 10)     # shape: (1, 20, 10)
 
-# Run model prediction
+# Run prediction
 activity_pred, emotion_pred, sleep_pred = model.predict(sequence)
 
-# Create output JSON
+# Format predictions into structured JSON
 result = {
     "activity": [
         {
@@ -87,4 +51,5 @@ result = {
     }
 }
 
+# Print JSON to stdout (Node.js backend will read this)
 print(json.dumps(result))
